e10dcd368a360685f0d8e0a71c57e4c25ba7c263
maj0rkeen <iamsparshmishra@gmail.com>
2026-06-08 22:03:57 +0530

    cpp

---
+++ C++ Files/typedef.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+typedef int Integer;
+//typedef= keyword used to create an alias for a data type or a user-defined type.
+int main()
+{
+    Integer x = 5;
+    std::cout << x << std::endl;
+    return 0;
+} 
