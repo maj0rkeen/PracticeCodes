0885623b4a3c470ba072cc07c210260d012e4a1f
sparsh-1703 <iamsparshmishra@gmail.com>
2026-09-10 23:52:50 +0530

    cfsh1

---
+++ Codeforces/K_Max_and_Min.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int mn = a;
+    if (b < mn) mn = b;
+    if (c < mn) mn = c;
+
+    int mx = a;
+    if (b > mx) mx = b;
+    if (c > mx) mx = c;
+
+    cout << mn << " " << mx << endl;
+
+    return 0;
+}
